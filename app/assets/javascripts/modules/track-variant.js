c5256168300f3f72945e4573e1ad1eaa08527b1b
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2021-01-14 16:54:50 +0000

    Fix linting

--- app/assets/javascripts/modules/track-variant.js
+++ app/assets/javascripts/modules/track-variant.js
@@ -1,7 +1,7 @@
 window.GOVUK.Modules = window.GOVUK.Modules || {};
 
 (function(Modules) {
-  "use strict";
+  'use strict'
 
   Modules.TrackVariant = function () {
     this.start = function ($element) {
@@ -17,15 +17,15 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         var cookielessTracker = new GOVUK.Modules.CookielessTracker('UA-26179049-29', {
           name: 'CookielessTracker',
           storage: 'none',
-          clientId: '0',
-        });
+          clientId: '0'
+        })
 
         cookielessTracker.trackEvent('cookieless', 'hit', {
           trackerName: 'CookielessTracker',
           label: variant,
           javaEnabled: false,
           language: ''
-        });
+        })
       }
     }
   }
--- spec/javascripts/track-variant-spec.js
+++ spec/javascripts/track-variant-spec.js
@@ -1,61 +1,61 @@
-describe('Test variant tracker', function() {
-  "use strict";
+describe('Test variant tracker', function () {
+  "use strict"
 
   var tracker,
     element,
     FakeCookielessTracker,
-    gaSpy;
+    gaSpy
 
-  beforeEach(function() {
+  beforeEach(function () {
     GOVUK.cookie('cookies_preferences_set', null)
-    gaSpy = jasmine.createSpyObj('initGa', ['send']);
+    gaSpy = jasmine.createSpyObj('initGa', ['send'])
 
-    FakeCookielessTracker = function(trackingId, fieldsObject) {}
-    FakeCookielessTracker.prototype.trackEvent = function(category, action, options) {
-      gaSpy.send(category, action, options);
+    FakeCookielessTracker = function (trackingId, fieldsObject) {}
+    FakeCookielessTracker.prototype.trackEvent = function (category, action, options) {
+      gaSpy.send(category, action, options)
     }
 
-    GOVUK.Modules.CookielessTracker = FakeCookielessTracker;
+    GOVUK.Modules.CookielessTracker = FakeCookielessTracker
 
-    tracker = new GOVUK.Modules.TrackVariant();
+    tracker = new GOVUK.Modules.TrackVariant()
   });
 
-  afterEach(function() {
+  afterEach(function () {
     GOVUK.Modules.CookielessTracker = null
-  });
+  })
 
-  it('tracks A variant', function() {
-    element = $('<meta name="Cookieless-Variant" content="A" data-module="track-variant">');
+  it('tracks A variant', function () {
+    element = $('<meta name="Cookieless-Variant" content="A" data-module="track-variant">')
 
-    tracker.start(element);
+    tracker.start(element)
 
     expect(gaSpy.send).toHaveBeenCalledWith('cookieless', 'hit', {
       trackerName: 'CookielessTracker',
       label: 'A',
       javaEnabled: false,
       language: ''
-    });
-  });
+    })
+  })
 
-  it('tracks B variant', function() {
-    element = $('<meta name="Cookieless-Variant" content="B" data-module="track-variant">');
+  it('tracks B variant', function () {
+    element = $('<meta name="Cookieless-Variant" content="B" data-module="track-variant">')
 
-    tracker.start(element);
+    tracker.start(element)
 
     expect(gaSpy.send).toHaveBeenCalledWith('cookieless', 'hit', {
       trackerName: 'CookielessTracker',
       label: 'B',
       javaEnabled: false,
       language: ''
-    });
-  });
+    })
+  })
 
-  it('does not track variant if cookie is set', function() {
+  it('does not track variant if cookie is set', function () {
     GOVUK.cookie('cookies_preferences_set', true)
-    element = $('<meta name="Cookieless-Variant" content="Z" data-module="track-variant">');
+    element = $('<meta name="Cookieless-Variant" content="Z" data-module="track-variant">')
 
-    tracker.start(element);
+    tracker.start(element)
 
-    expect(gaSpy.send).not.toHaveBeenCalled();
-  });
-});
+    expect(gaSpy.send).not.toHaveBeenCalled()
+  })
+})
